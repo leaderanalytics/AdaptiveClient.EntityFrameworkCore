ef4540e61dde5f767ba12da1e01c99d4b30928e0
Sam <sam.wheat@outlook.com>
2018-06-05 20:57:44 -0700

    Add excluded files

---
+++ LeaderAnalytics.AdaptiveClient.EntityFramework.Tests/Artifacts/BackOffice/BOServiceManifest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeaderAnalytics.AdaptiveClient.EntityFramework;
+
+namespace LeaderAnalytics.AdaptiveClient.EntityFramework.Tests.Artifacts.BackOffice
+{
+    public class BOServiceManifest : ServiceManifestFactory, IBOServiceManifest
+    {
+        public IAccountsService AccountsService { get => Create<IAccountsService>(); }
+        public IPaymentsService PaymentsService { get => Create<IPaymentsService>(); }
+    }
+}
---
+++ LeaderAnalytics.AdaptiveClient.EntityFramework.Tests/Artifacts/BackOffice/IBOServiceManifest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeaderAnalytics.AdaptiveClient.EntityFramework.Tests.Artifacts.BackOffice
+{
+    public interface IBOServiceManifest : IDisposable
+    {
+        IPaymentsService PaymentsService { get; }
+        IAccountsService AccountsService { get; }
+    }
+}
