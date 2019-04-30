9a30f0e0ded2b91941095d73298acda4f301d7bf
TEFI005 <sam.wheat@janus.com>
2019-04-30 07:19:16 -0700

    Add missing files

---
+++ LeaderAnalytics.AdaptiveClient.EntityFrameworkCore.Tests/Artifacts/StoreFront/SFServiceManifest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeaderAnalytics.AdaptiveClient.EntityFrameworkCore.Tests.Artifacts.StoreFront
+{
+    public class SFServiceManifest : ServiceManifestFactory, ISFServiceManifest
+    {
+        public IOrdersService OrdersService { get => Create<IOrdersService>(); }
+        public IProductsService ProductsService { get => Create<IProductsService>(); }
+    }
+}
