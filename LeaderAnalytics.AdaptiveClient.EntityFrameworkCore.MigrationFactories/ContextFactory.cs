599c88fd75617b06f0ede9649a65d377123e2883
Sam Wheat <sam.wheat@outlook.com>
2024-12-03 17:52:25 -0800

    Update tests

--- LeaderAnalytics.AdaptiveClient.EntityFrameworkCore.MigrationFactories/ContextFactory.cs
+++ LeaderAnalytics.AdaptiveClient.EntityFrameworkCore.MigrationFactories/ContextFactory.cs
@@ -8,53 +8,56 @@ using Pomelo.EntityFrameworkCore.MySql;
 using LeaderAnalytics.AdaptiveClient.EntityFrameworkCore.Tests;
 using LeaderAnalytics.AdaptiveClient.EntityFrameworkCore.Tests.Artifacts;
 
-namespace LeaderAnalytics.AdaptiveClient.EntityFrameworkCore.MigrationFactories
+namespace LeaderAnalytics.AdaptiveClient.EntityFrameworkCore.MigrationFactories;
+public static class Consts
 {
-    public class BackOffice_MSSQLContextFactory : IDesignTimeDbContextFactory<Tests.Artifacts.BackOffice.Db_MSSQL>
-    {
-        public Tests.Artifacts.BackOffice.Db_MSSQL CreateDbContext(string[] args)
-        {
-            string connectionString = ConnectionstringUtility.GetConnectionString("bin\\debug\\netcoreapp2.0\\EndPoints.json", API_Name.BackOffice, DataBaseProviderName.MSSQL);
-            DbContextOptionsBuilder dbOptions = new DbContextOptionsBuilder();
-            dbOptions.UseSqlServer(connectionString);
-            Tests.Artifacts.BackOffice.Db_MSSQL db = new Tests.Artifacts.BackOffice.Db_MSSQL(dbOptions.Options);
-            return db;
-        }
-    }
+    public const string endPointsFile = "bin\\debug\\netcoreapp2.0\\EndPoints.json";
+}
 
-    public class BackOffice_MySQLContextFactory : IDesignTimeDbContextFactory<Tests.Artifacts.BackOffice.Db_MySQL>
+public class BackOffice_MSSQLContextFactory : IDesignTimeDbContextFactory<Tests.Artifacts.BackOffice.Db_MSSQL>
+{
+    public Tests.Artifacts.BackOffice.Db_MSSQL CreateDbContext(string[] args)
     {
-        public Tests.Artifacts.BackOffice.Db_MySQL CreateDbContext(string[] args)
-        {
-            string connectionString = ConnectionstringUtility.BuildConnectionString(ConnectionstringUtility.GetConnectionString("bin\\debug\\netcoreapp2.0\\EndPoints.json", API_Name.BackOffice, DataBaseProviderName.MySQL));
-            DbContextOptionsBuilder dbOptions = new DbContextOptionsBuilder();
-            dbOptions.UseMySql(ServerVersion.AutoDetect(connectionString));
-            Tests.Artifacts.BackOffice.Db_MySQL db = new Tests.Artifacts.BackOffice.Db_MySQL(dbOptions.Options);
-            return db;
-        }
+        string connectionString = EndPointUtilities.LoadEndPoints(Consts.endPointsFile, false).First(x => x.API_Name == API_Name.BackOffice && x.ProviderName == DataBaseProviderName.MSSQL).ConnectionString;
+        DbContextOptionsBuilder dbOptions = new DbContextOptionsBuilder();
+        dbOptions.UseSqlServer(connectionString);
+        Tests.Artifacts.BackOffice.Db_MSSQL db = new Tests.Artifacts.BackOffice.Db_MSSQL(dbOptions.Options);
+        return db;
     }
+}
 
-    public class StoreFront_MSSQLContextFactory : IDesignTimeDbContextFactory<Tests.Artifacts.StoreFront.Db_MSSQL>
+public class BackOffice_MySQLContextFactory : IDesignTimeDbContextFactory<Tests.Artifacts.BackOffice.Db_MySQL>
+{
+    public Tests.Artifacts.BackOffice.Db_MySQL CreateDbContext(string[] args)
     {
-        public Tests.Artifacts.StoreFront.Db_MSSQL CreateDbContext(string[] args)
-        {
-            string connectionString = ConnectionstringUtility.GetConnectionString("bin\\debug\\netcoreapp2.0\\EndPoints.json", API_Name.StoreFront, DataBaseProviderName.MSSQL);
-            DbContextOptionsBuilder dbOptions = new DbContextOptionsBuilder();
-            dbOptions.UseSqlServer(connectionString);
-            Tests.Artifacts.StoreFront.Db_MSSQL db = new Tests.Artifacts.StoreFront.Db_MSSQL(dbOptions.Options);
-            return db;
-        }
+        string connectionString = EndPointUtilities.LoadEndPoints(Consts.endPointsFile, false).First(x => x.API_Name == API_Name.BackOffice && x.ProviderName == DataBaseProviderName.MySQL).ConnectionString;
+        DbContextOptionsBuilder dbOptions = new DbContextOptionsBuilder();
+        dbOptions.UseMySql(ServerVersion.AutoDetect(connectionString));
+        Tests.Artifacts.BackOffice.Db_MySQL db = new Tests.Artifacts.BackOffice.Db_MySQL(dbOptions.Options);
+        return db;
     }
+}
 
-    public class StoreFront_MySQLContextFactory : IDesignTimeDbContextFactory<Tests.Artifacts.StoreFront.Db_MySQL>
+public class StoreFront_MSSQLContextFactory : IDesignTimeDbContextFactory<Tests.Artifacts.StoreFront.Db_MSSQL>
+{
+    public Tests.Artifacts.StoreFront.Db_MSSQL CreateDbContext(string[] args)
     {
-        public Tests.Artifacts.StoreFront.Db_MySQL CreateDbContext(string[] args)
-        {
-            string connectionString = ConnectionstringUtility.BuildConnectionString(ConnectionstringUtility.GetConnectionString("bin\\debug\\netcoreapp2.0\\EndPoints.json", API_Name.StoreFront, DataBaseProviderName.MySQL));
-            DbContextOptionsBuilder dbOptions = new DbContextOptionsBuilder();
-            dbOptions.UseMySql(ServerVersion.AutoDetect(connectionString));
-            Tests.Artifacts.StoreFront.Db_MySQL db = new Tests.Artifacts.StoreFront.Db_MySQL(dbOptions.Options);
-            return db;
-        }
+        string connectionString = EndPointUtilities.LoadEndPoints(Consts.endPointsFile, false).First(x => x.API_Name == API_Name.StoreFront && x.ProviderName == DataBaseProviderName.MSSQL).ConnectionString;
+        DbContextOptionsBuilder dbOptions = new DbContextOptionsBuilder();
+        dbOptions.UseSqlServer(connectionString);
+        Tests.Artifacts.StoreFront.Db_MSSQL db = new Tests.Artifacts.StoreFront.Db_MSSQL(dbOptions.Options);
+        return db;
+    }
+}
+
+public class StoreFront_MySQLContextFactory : IDesignTimeDbContextFactory<Tests.Artifacts.StoreFront.Db_MySQL>
+{
+    public Tests.Artifacts.StoreFront.Db_MySQL CreateDbContext(string[] args)
+    {
+        string connectionString = EndPointUtilities.LoadEndPoints(Consts.endPointsFile, false).First(x => x.API_Name == API_Name.StoreFront && x.ProviderName == DataBaseProviderName.MySQL).ConnectionString;
+        DbContextOptionsBuilder dbOptions = new DbContextOptionsBuilder();
+        dbOptions.UseMySql(ServerVersion.AutoDetect(connectionString));
+        Tests.Artifacts.StoreFront.Db_MySQL db = new Tests.Artifacts.StoreFront.Db_MySQL(dbOptions.Options);
+        return db;
     }
 }
